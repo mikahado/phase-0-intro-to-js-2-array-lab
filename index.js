bad5f36f700ad90e0425964e68b40e4e9a02881f
Michael <mikahado@gmail.com>
2022-09-01 00:03:44 -0400

    commit tree 064c01cf6e0fb0207579133a5643828aa872c56c

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyAppendCat(name){
+    return cats.push(name)
+}
+
+function destructivelyPrependCat(name) {
+    return cats.unshift(name)
+
+}
+
+function destructivelyRemoveLastCat(){
+    return cats.pop()
+
+}
+
+function destructivelyRemoveFirstCat() {
+    return cats.shift()
+}
+
+function appendCat(name) {
+    const allCats = [...cats, name]
+    return allCats
+}
+
+function prependCat(name) {
+    const allCats = [name, ...cats]
+    return allCats
+
+}
+
+function removeLastCat() {
+    return cats.slice(0, -1);
+}
+
+function removeFirstCat() {
+    return cats.slice(1)
+
+}
